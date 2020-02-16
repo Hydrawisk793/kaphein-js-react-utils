6955e8bf9ec247cd21bd83eb3cf3704d9126d424
Hydrawisk793 <hyw793@gmail.com>
2020-02-16 12:32:00 +0900

    Removed 'dist' directory from the git repository.

--- dist/generate-random-key.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function generateRandomKey(
-    length : number
-) : string;
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { generateRandomKey } from "./generate-random-key";
-
-export { ReactContextValueType } from "./ts-utils";
-export { createSimpleContextEnhancer } from "./create-simple-context-enhancer";
