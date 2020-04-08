dac2a9cc31edfd7f31921075b7dfe67de8040d0d
Hydrawisk793 <hyw793@gmail.com>
2020-04-09 01:40:59 +0900

    fix: fix wrong rem calculation.

--- src/size-unit-context/components/browser-size-unit-provider.jsx
+++ src/size-unit-context/components/browser-size-unit-provider.jsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useRef, useMemo, useState } from "react";
+import React, { memo, useCallback, useRef, useMemo, useState } from "react";
 import PropTypes from "prop-types";
-import { useComponentMountEffects } from "../../hooks-utils";
+import { useComponentDidMount, useComponentWillUnmount, useMutationObserver } from "../../hooks-utils";
 
 import { SizeUnitContext } from "../size-unit-context";
 
@@ -14,6 +14,11 @@ export const BrowserSizeUnitProvider = (function ()
      *  @typedef {typeof import("../size-unit-context")["defaultValue"]} ContextValue
      */
 
+    const remMutationOption = {
+        attributes : true,
+        attributeFilter : ["style"],
+    };
+
     /**
      *  @param {OwnProps} props
      */
@@ -24,59 +29,65 @@ export const BrowserSizeUnitProvider = (function ()
         } = props;
 
         const isWindowExist = useRef("undefined" !== typeof window);
+        isWindowExist.current = "undefined" !== typeof window;
         const [windowWidth, setWindowWidth] = useState((isWindowExist.current ? window.innerWidth : 0));
         const [windowHeight, setWindowHeight] = useState((isWindowExist.current ? window.innerHeight : 0));
+        const [rem, setRem] = useState(0);
 
         const onWindowResize = useCallback(
-            () =>
+            function ()
             {
                 setWindowWidth((isWindowExist.current ? window.innerWidth : 0));
                 setWindowHeight((isWindowExist.current ? window.innerHeight : 0));
             },
             []
-        )
-        const didMount = useCallback(
-            () =>
-            {
-                if(isWindowExist.current) {
-                    window.addEventListener("resize", onWindowResize);
-                }
-            },
-            [
-                onWindowResize,
-            ]
         );
-        const willUnmount = useCallback(
-            () =>
-            {
-                if(isWindowExist.current) {
-                    window.removeEventListener("resize", onWindowResize);
-                }
-            },
-            [
-                onWindowResize,
-            ]
-        );
-
-        useComponentMountEffects(didMount, willUnmount);
 
         const contextValue = useMemo(
-            () =>
+            function ()
             {
                 return /** @type {ContextValue} */({
                     vw : windowWidth * 0.01,
                     vh : windowHeight * 0.01,
-                    rem : (
-                        isWindowExist.current && "undefined" !== typeof window.document
-                        ? parseFloat(getComputedStyle(document.documentElement).fontSize)
-                        : 0
-                    ),
+                    rem : rem,
                 });
             },
             [
                 windowWidth,
                 windowHeight,
+                rem,
             ]
+        );
+
+        useComponentDidMount(
+            function ()
+            {
+                if(isWindowExist.current) {
+                    window.addEventListener("resize", onWindowResize);
+
+                    setRem(Number.parseFloat(getComputedStyle(document.documentElement).fontSize));
+                }
+            }
+        );
+        useComponentWillUnmount(
+            function ()
+            {
+                if(isWindowExist.current) {
+                    window.removeEventListener("resize", onWindowResize);
+                }
+            }
+        );
+        useMutationObserver(
+            document.documentElement,
+            remMutationOption,
+            /**
+             *  @type {Parameters<typeof useMutationObserver>["2"]}
+             */
+            function (mutation)
+            {
+                /** @type {HTMLElement} */const target = mutation.target;
+                setRem(Number.parseFloat(target.style.fontSize));
+            }
         );
 
         return (
@@ -96,5 +107,5 @@ export const BrowserSizeUnitProvider = (function ()
 
     BrowserSizeUnitProvider.whyDidYouRender = true;
 
-    return BrowserSizeUnitProvider;
+    return memo(BrowserSizeUnitProvider);
 })();
