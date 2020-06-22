e70d416e3290b0fe117403eb2f4a8f71c5f99596
Hydrawisk793 <hyw793@gmail.com>
2020-06-22 16:02:10 +0900

    fix: fix the wrong parameter handling of usePrevious hook.

--- src/hooks-utils.js
+++ src/hooks-utils.js
@@ -13,7 +13,7 @@ import { isFunction } from "kaphein-js";
  */
 export function usePrevious(value)
 {
-    var prevValueRef = useRef(arguments[1]);
+    var prevValueRef = useRef((arguments.lenght > 1 ? arguments[1] : value));
 
     useEffect(
         function ()
